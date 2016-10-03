4be35d21795693da73ec154ba6006ee2acb10002
Anna Kasunic <amkasunic@gmail.com>
2016-10-03 04:17:59 -0400

    no message

---
+++ myscript.js
@@ -0,0 +1,55 @@
+//------------------PART 1 CODE BELOW HERE-----------------------
+
+
+
+//-------------------PART 2 CODE BELOW HERE-------------------
+
+//The following code is for part 2 of the tutorial. Do not uncomment this part until you get to the traversing the DOM section!
+
+/*
+$('.panel').append($('<h3>').append('Click on a person to make their wishes come true. Reload the page to start over.'))
+        .append($('<ul>')
+            .append($('<li>').attr("id", "gma").text('Grandma: “Please get rid of my entire family, including me.”'))
+            .append($('<li>').attr("id", "gpa").text('Grandpa: "Can you put a nurse between my wife and me help take care of us?”'))
+            .append($('<ul>')
+                .append($('<li>').attr("id", "mom").text('Mom: “I would love to have another daughter.”'))
+                .append($('<li>').attr("id", "dad").text('Dad: “I don’t want the odd-numbered children.”'))
+                .append($('<ul>')
+                    .append($('<li>').attr("id", "bro1").text('Brother 1: "I definitely want to get rid of my sister.”')) 
+                    .append($('<li>').attr("id", "sis").text('Sister: “Please get rid of my parents.”'))
+                    .append($('<li>').attr("id", "bro2").text('Brother 2: “Let’s get rid of adults altogether (parents and grandparents)."'))
+                )
+            )
+        )
+    ;
+    
+
+$('li').click(function(){
+    var person = $(this).attr('id');
+    var item = $(this); //storing in a variable to make it easier later
+    console.log(person);
+    switch(person){
+        case "sis":
+            //Your code here
+            break;
+        case "bro1":
+            //Your code here
+            break;
+        case "bro2":
+            //Your code here
+            break;
+        case "mom":
+            //Your code here
+            break;
+        case "dad":
+            //Your code here
+            break;
+        case "gma":
+            //Your code here
+            break;
+        case "gpa":
+            //Your code here
+    }
+});
+
+*/
